6ced8367cc7290489967b5d9f4d83b14fda6adad
Trennig <basheermujavar@gmail.com>
2020-06-11 22:32:57 +0530

    no message

---
+++ JailBreak/Assets/Scripts/Misc/TextAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+
+    int currentIndex = 0;
+    Image textImage;
+    DateTime startTime;
+
+    private void Start()
+    {
+        currentIndex = 0;
+        textImage = GetComponent<Image>();
+        textImage.sprite = sprites[currentIndex];
+        startTime = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        if((DateTime.Now - startTime).TotalMilliseconds >= 450)
+        {
+            currentIndex++;
+            textImage.sprite = sprites[currentIndex];
+            startTime = DateTime.Now;
+
+            if (currentIndex == sprites.Length - 1)
+                currentIndex = -1;
+        }
+    }
+}
